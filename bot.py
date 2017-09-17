28d7f45b562d09e1dd9e5843a4d644e7f84c9cee
Ricardo Ruiz <rjruizfdez@gmail.com>
2017-09-17 18:44:30 +0200

    Little bug fixed

--- bot.py
+++ bot.py
@@ -2,12 +2,14 @@ import os, sys, time
 from random import randint
 
 
+os.system("python main.py -u "+sys.argv[3]+" -p "+sys.argv[4]+" -o super-followback")
+
+
 while(1):
 	try:
 		secs = randint(int(sys.argv[1]), int(sys.argv[2]))
 		time.sleep(secs)
 		os.system("python main.py -u "+sys.argv[3]+" -p "+sys.argv[4]+" -o follow-location -t 127963847 ")
-		os.system("python main.py -u "+sys.argv[3]+" -p "+sys.argv[4]+" -o super-followback")
 	except:
 		pass
 
